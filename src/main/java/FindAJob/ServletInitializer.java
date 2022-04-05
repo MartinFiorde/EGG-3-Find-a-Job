56d4a7f44e3482c402c2db06625f7ce4b54c83a7
Matias Villarruel <matiasnicolasvillarruel99@gmail.com>
2022-04-04 22:07:56 -0300

    creacion de enums de Zonas

--- src/main/java/FindAJob/ServletInitializer.java
+++ src/main/java/FindAJob/ServletInitializer.java
@@ -7,7 +7,11 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
+		
+            
+            
+            
+            return application.sources(Application.class);
 	}
 
 }
